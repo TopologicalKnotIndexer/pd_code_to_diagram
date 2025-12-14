0f8fde25e67ffc8cafa02dcdc70ca61d195ec0d9
GGN_2015 <neko@jlulug.org>
2025-12-15 01:55:48 +0800

    v2025-12-15

--- src/main.cpp
+++ src/main.cpp
@@ -13,6 +13,7 @@
 #include "PDTreeAlgo/PDTree.h"
 #include "PDTreeAlgo/SocketInfo.h"
 
+const int DEBUG = 0;
 #ifndef NO_MAIN // 如果 NO_MAIN 标志存在，则不编译 main 函数
 int main() {
 
@@ -20,11 +21,11 @@ int main() {
     unsigned int seed = 42;
     random::setSeed(seed);
 
-    std::cout << "input pd_code ..." << std::endl;
+    if(DEBUG) std::cout << "input pd_code ..." << std::endl;
     PDCode pd_code;
     pd_code.InputPdCode(std::cin); // 输入一个 pd_code
 
-    std::cout << "generating pd_tree ..." << std::endl;
+    if(DEBUG) std::cout << "generating pd_tree ..." << std::endl;
     PDTree pd_tree;
 
     // 生成树形图直到没有交叉点重叠
@@ -36,17 +37,17 @@ int main() {
         }
     }
 
-    std::cout << "generating and checking socket_info ..." << std::endl;
+    if(DEBUG) std::cout << "generating and checking socket_info ..." << std::endl;
     SocketInfo s_info = pd_tree.getSocketInfo(); // 生成完全的插头信息
     s_info.check(pd_code.getCrossingNumber());   // 检查信息合法性
 
-    std::cout << "running link algo ..." << std::endl;
+    if(DEBUG) std::cout << "running link algo ..." << std::endl;
     LinkAlgo link_algo(pd_code.getCrossingNumber(), s_info);
-    link_algo.getFinalGraph().debugOutput();
+    // link_algo.getFinalGraph().debugOutput(); // 输出二维布局图
 
-    std::cout << "output ans ..." << std::endl;
+    if(DEBUG) std::cout << "output ans ..." << std::endl;
     GenNodeSetAlgo gen_node_set_algo(link_algo.getFinalGraph(), link_algo.getAllEdges());
-    gen_node_set_algo.outputGraph(std::cout);
+    gen_node_set_algo.outputGraph(std::cout); // 输出三维点坐标情况
     return 0;
 }
 #endif
